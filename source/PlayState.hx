4e6757a7c90398309efc88bc0fed977c15c94d80
DaveNewell <eduardito352@msn.com>
2018-12-07 19:50:13 -0400

    im so fresh you can suck my nuts

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,7 +5,7 @@ import flixel.FlxState;
 class PlayState extends FlxState {
 	override public function create():Void {
 		super.create();
-		var text = new flixel.text.FlxText(0, 0, 0, "UnderTale", 64);
+		var text = new flixel.text.FlxText(0, 0, 0, "im so fresh you can suck my nuts", 64);
 		text.screenCenter();
 		add(text);
 	}
