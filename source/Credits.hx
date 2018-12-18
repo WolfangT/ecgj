13f684d55f864dc5dce73198fd4d2ee0e0fdff8f
Wolfang Torres <wolfang.torres@gmail.com>
2018-12-18 02:38:21 -0400

    fix credits

--- source/Credits.hx
+++ source/Credits.hx
@@ -32,9 +32,9 @@ class Credits extends FlxState {
 		_bg = new FlxSprite(0, 0, AssetPaths.main__png);
 		_btnPlay = new FlxButton(FlxG.width * 0.5, FlxG.height * 0.9, "Return", clickPlay);
 		_name = new FlxText(FlxG.width * 0.4, FlxG.height * 0.10, 0, "Credits", 50); // "The great Nort Pole holiday bulglary of 82!"
-		_textCredits1 = new FlxText(FlxG.width * 0.10, FlxG.height * 0.35, 0, "Developed by Wolfang Torres and Eduardo Rivero", 20);
+		_textCredits1 = new FlxText(FlxG.width * 0.10, FlxG.height * 0.35, 0, "Developed by Wolfang Torres and Eduardo Rivero", 19);
 		_textCredits2 = new FlxText(FlxG.width * 0.10, FlxG.height * 0.45, 0, "Original art by https://opengameart.org/content/a-puzzling-present-image-bank",
-			12);
+			11);
 		_textCredits3 = new FlxText(FlxG.width * 0.10, FlxG.height * 0.50, 0, "Sounds Efects created with http://sfbgames.com/chiptone/", 15);
 		add(_bg);
 		add(_name);
